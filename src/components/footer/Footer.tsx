c702beee13bf39277fb4f8f549c6e867c153bd82
CiccioTecchio <fr.vicidomini94@gmail.com>
2022-03-12 14:49:33 +0100

    footer -refs #5 added sticky footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,12 +2,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import React from 'react';
 import "./Footer.css"
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
 function Footer(){
+  const faPropIcon = faGithub as IconProp
   return(
-    <div className="row footer-content">
-      <div className="col-md-12">
-        <FontAwesomeIcon icon={faGithub} />
+    <div className="footer-content">
+      <div className="row">
+        <div className="col-md-12">
+          <FontAwesomeIcon icon={faPropIcon} />
+        </div>
       </div>
     </div>
   )
