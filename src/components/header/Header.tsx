d2281583f19d7db37ac74b594041b43ea873ffe2
CiccioTecchio <fr.vicidomini94@gmail.com>
2022-03-12 14:48:23 +0100

    header -refs #4 added CV

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,20 +1,25 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faFileDownload} from '@fortawesome/free-solid-svg-icons'
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import "./Header.css"
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
 function Header(){
   const {t,i18n} = useTranslation();
-
   return(
     <div className="row header-content">
-      <div className="col-md-10">
-        <h1>Francesco Vicidomini</h1>
+      <div className="col-md-9">
+        <h1>{t('header.title')}</h1>
       </div>
       <div className="col-md-1">
         <h1><span className="lang" onClick={()=> i18n.changeLanguage("it")}>🇮🇹</span></h1>
       </div>
       <div className="col-md-1">
         <h1><span className="lang" onClick={()=> i18n.changeLanguage("en")}>🇬🇧</span></h1>
+      </div>
+      <div className="col-md-1">
+        <h1><span className='lang' title={t('header.download.cv')} > <FontAwesomeIcon icon={faFileDownload as IconProp} /> </span></h1>
       </div>
     </div>
   )
