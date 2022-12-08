be27072e20a74c84e879ee8946da69e97a5cf2ce
CiccioTecchio <fr.vicidomini94@gmail.com>
2022-12-08 21:26:53 +0100

    project -refs #6 added slider project

--- src/components/project/Project.tsx
+++ src/components/project/Project.tsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { faArrowCircleLeft, faArrowCircleRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
 import { Button, Card, Col, Row } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import "./Project.css"
@@ -12,6 +15,9 @@ interface IProject{
 
 function Project(){
   const {t} = useTranslation();
+  const [idx, setIdx] = useState(0);
+  const [disablePrev, setDisablePrev] = useState(true);
+  const [disableNext, setDisableNext] = useState(false);
   
   const projectList:Array<IProject> = [
     {key: "PAB", title:t("project.pab.title"), description:t("project.pab.desc")},
@@ -25,59 +31,65 @@ function Project(){
     {key: "tesi-triennale", title:t("project.thesis2.title"), description:t("project.thesis2.desc"), repository: t("project.thesis2.repo")},
     {key: "cicciotecchio-io", title:t("project.cicciotecchio-io.title"), description: t("project.cicciotecchio-io.desc"), repository: t("project.cicciotecchio-io.repo")},
     {key: "unisa-latex", title:t("project.unisa-latex.title"), description: t("project.unisa-latex.desc"), repository: t("project.unisa-latex.repo")},
-    {key: "my-piggy-bank", title:t("project.my-piggy-bank.title"), description: t("project.my-piggy-bank.desc")}
-
   ];
+
+  const goBack = () =>{
+    if(idx > 0) {
+      setIdx(idx-1); setDisablePrev(false);
+    } else setDisablePrev(true);
+    setDisableNext(false);
+  }
+
+  const goNext = () => {
+    if(idx < projectList.length -2){
+      setIdx(idx+1); setDisableNext(false)
+    }else setDisableNext(true);
+    setDisablePrev(false);
+  }
+
   return(
     <div className="mt-3">
       <Row>
       <Col className="text-center"><h1>{t("project.title")}</h1></Col>
     </Row>
-    
-      {writeRowProject(projectList)}
-    
+      <Row>
+        <Col md={1} className='align-self-center'>
+          <Button variant='outline-secondary' size='lg' onClick={goBack} disabled={disablePrev}>
+            <FontAwesomeIcon icon={faArrowCircleLeft as IconProp}/>
+          </Button>
+        </Col>
+        <Col md={5}>
+          <Card className='mt-3 h-100'>
+            <Card.Body className='text-center'>
+              <Card.Title><b>{projectList[idx].title}</b></Card.Title>
+              <Card.Text dangerouslySetInnerHTML={{__html:projectList[idx].description}} ></Card.Text>
+            </Card.Body>
+            {(projectList[idx].repository)?writeCardFooterForProject(projectList[idx].repository):<></>}
+          </Card>
+        </Col>
+        <Col md={5}>
+          <Card className='mt-3 h-100'>
+            <Card.Body className='text-center'>
+              <Card.Title><b>{projectList[idx+1].title}</b></Card.Title>
+              <Card.Text dangerouslySetInnerHTML={{__html:projectList[idx+1].description}} ></Card.Text>
+            </Card.Body>
+            {(projectList[idx+1].repository)?writeCardFooterForProject(projectList[idx+1].repository):<></>}
+          </Card>
+        </Col>
+        <Col md={1} className='align-self-center'>
+          <Button variant='outline-secondary' size='lg' onClick={goNext} disabled={disableNext}>
+            <FontAwesomeIcon icon={faArrowCircleRight as IconProp}/>
+            </Button>
+        </Col>
+      </Row>
     </div>
   )
 }
 
-function writeColumnProjectList(projectList:Array<IProject>):Array<any>{
-  return projectList.map((project:IProject, idx:number)=>{
-     return <Col key={project.key} md={4}>
-              <Card className='mt-3 h-100'>
-                <Card.Body className='text-center'>
-                  <Card.Title><b>{project.title}</b></Card.Title>
-                  <Card.Text dangerouslySetInnerHTML={{__html:project.description}} ></Card.Text>
-                </Card.Body>
-                {(project.repository)?writeCardFooterForProject(project.repository):<></>}
-              </Card>
-            </Col>
-  })
-}
-
-function writeCardFooterForProject(repo:string){
+function writeCardFooterForProject(repo:string|undefined){
   return <Card.Footer className='text-center' style={{background: 'none', border: '0'}}>
     <Button variant='primary' href={repo} target="_blank">Repository</Button>
   </Card.Footer>
-}
-
-function writeRowProject(projectList:Array<IProject>){
-  let rows:Array<any>= []; let cols:Array<any> = writeColumnProjectList(projectList);
-  console.log(cols.length);
-  let toRender: Array<any> =  [];
-  cols.forEach((card:any,i:number) =>{
-    toRender.push(card)
-    if((i+1)%3 === 0){
-      rows.push(<Row  key={i} className='mt-4'> {toRender}</Row>)
-      toRender = []
-    }else{
-      const resto = (i+1) % 3;
-      if( (cols.length - (i+1)) < resto ){
-
-      }
-    }
-  })
-  console.log(rows)
-  return rows
 }
 
 export default Project;
